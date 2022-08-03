2e99a58afac51e0bd6214d85369e30381132aed3
Paulo Teixeira <pteixeira.dev@gmail.com>
2022-08-02 21:21:58 -0300

    [1] - Fix unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import axios from "axios";
 import App from "./App";
 
-test("renders hello world", () => {
+jest.mock("axios");
+const mockedAxiosGet = axios.get as jest.MockedFunction<typeof axios.get>;
+
+test("renders hello world", async () => {
+  mockedAxiosGet.mockResolvedValue({ data: "Hello world!" });
+
   render(<App />);
-  const linkElement = screen.getByText("Hello world!");
+  const linkElement = await screen.findByText("Hello world!");
   expect(linkElement).toBeInTheDocument();
 });
